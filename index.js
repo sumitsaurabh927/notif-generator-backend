32b6e4eb825d83f492c83eef6f4467d58288a08b
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2023-05-02 12:18:36 +0530

    backend done

--- index.js
+++ index.js
@@ -3,24 +3,29 @@ import mongoose from "mongoose"
 import cors from "cors"
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-
+import notifRoute from "./routes/notif.js"
 
 dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use("/home", notifRoute)
+
 app.listen(3000, function () {
     console.log('listening on 3000')
-  })
+})
 
-  app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('Project running!')
-  })
+})
 
-const connecturl = 'mongodb+srv://testuser:testpassword@cluster0.2ythvyv.mongodb.net/?retryWrites=true&w=majority'
 
-const CONNECTION_URL=connecturl;
-  const PORT = process.env.PORT || 8000
-  mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
-    .then(()=>app.listen(PORT,()=>console.log(`server running on port: ${PORT}`)))
-    .catch((error)=>console.log(error));
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 8000
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error));
